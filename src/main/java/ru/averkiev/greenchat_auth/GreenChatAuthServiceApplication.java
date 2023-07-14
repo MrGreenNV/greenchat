abfccd756c5e28201ee2251722c3d875d67ff8b7
MrGreenNV <averkievnv@gmail.com>
2023-07-14 13:03:56 +0300

    init

---
+++ src/main/java/ru/averkiev/greenchat_auth/GreenChatAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.averkiev.greenchat_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenChatAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenChatAuthServiceApplication.class, args);
+    }
+
+}
