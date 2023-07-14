0e9faebb506a5f6dad3d5748f620cdf7a5722a3c
MrGreenNV <averkievnv@gmail.com>
2023-07-14 21:16:16 +0300

    add RefreshToken.java

---
+++ src/main/java/ru/averkiev/greenchat_auth/models/RefreshToken.java
@@ -0,0 +1,33 @@
+package ru.averkiev.greenchat_auth.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "refresh_token")
+    private String accessToken;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "expires_at")
+    private Date expiresAt;
+}
