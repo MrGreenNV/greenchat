a576b3df6f8b30f374b7a73383d22564ed9dfd26
MrGreenNV <averkievnv@gmail.com>
2023-07-15 11:07:16 +0300

    update UserServiceClient.java add doc

--- src/main/java/ru/averkiev/greenchat_auth/services/UserServiceClient.java
+++ src/main/java/ru/averkiev/greenchat_auth/services/UserServiceClient.java
@@ -8,8 +8,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.averkiev.greenchat_auth.models.User;
 
+/**
+ * Класс представляет собой компонент микросервиса, отвечающий за взаимодействие с API стороннего микросервиса
+ * для получения данных о пользователе. Он предоставляет методы для выполнения запросов к API и получения
+ * информации о пользователе на остове его идентификатора.
+ * @author mrGreenNV
+ */
 @Component
 public class UserServiceClient {
+    /**
+     * HTTP-клиент, используемый для выполнения запросов к API стороннего микросервиса. Зависимость должна быть
+     * внедрена или передана в конструктор UserServiceClient.
+     */
     private final RestTemplate restTemplate;
     @Value("${user_management.url}")
     String apiUrl;
@@ -19,6 +29,11 @@ public class UserServiceClient {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     * Метод выполняет GET-запрос к API стороннего микросервиса для получения информации о пользователе по его имени.
+     * @param username - имя пользователя, для которого требуется получить информацию.
+     * @return - объект User, содержащий информацию о пользователе.
+     */
     public User getUserByUsername(String username) {
         ResponseEntity<User> responseEntity = restTemplate.exchange(
                 apiUrl,
