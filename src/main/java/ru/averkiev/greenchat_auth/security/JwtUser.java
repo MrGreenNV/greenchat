25cfe931199eaed087e0bb1af0c9663afc80e4c1
MrGreenNV <averkievnv@gmail.com>
2023-07-15 12:11:43 +0300

    add JwtUser.java

---
+++ src/main/java/ru/averkiev/greenchat_auth/security/JwtUser.java
@@ -0,0 +1,64 @@
+package ru.averkiev.greenchat_auth.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Класс предназначен для работы с JWT. Он предоставляет информацию о пользователе, необходимую для создания и проверки
+ * JWT токенов, а также для аутентификации и авторизации пользователей в greenchat.
+ * @author mrGreenNV
+ */
+@Data
+public class JwtUser implements UserDetails {
+    private final int id;
+    private final String username;
+    private final String password;
+    private final String firstname;
+    private final String lastname;
+    private final String email;
+    private final boolean enabled;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
