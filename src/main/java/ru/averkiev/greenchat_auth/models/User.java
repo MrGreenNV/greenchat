c1db78155c2e1c57f8c31f8646a73543a93719cf
MrGreenNV <averkievnv@gmail.com>
2023-07-15 13:28:40 +0300

    update User.java

--- src/main/java/ru/averkiev/greenchat_auth/models/User.java
+++ src/main/java/ru/averkiev/greenchat_auth/models/User.java
@@ -3,11 +3,21 @@ package ru.averkiev.greenchat_auth.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Set;
+
+/**
+ * Класс предсавляет собой модель пользователя в микросервисе greenchat_auth. он содержит информацию о пользователе.
+ * @author mrGrennNV
+ */
 @AllArgsConstructor
 @Data
 public class User {
     private int id;
     private String username;
     private String password;
+    private String firstname;
+    private String lastname;
     private String email;
+    private String status;
+    private Set<String> roles;
 }
