0e58fe5cbe79be91b455d7804e66f2d33a54d14b
MrGreenNV <averkievnv@gmail.com>
2023-07-15 14:23:36 +0300

    add JwtUserDetailsServiceTest.java

---
+++ src/test/java/ru/averkiev/greenchat_auth/security/JwtUserDetailsServiceTest.java
@@ -0,0 +1,85 @@
+package ru.averkiev.greenchat_auth.security;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.averkiev.greenchat_auth.models.User;
+import ru.averkiev.greenchat_auth.services.UserServiceClient;
+
+import java.util.Set;
+
+/**
+ * Тестовый класс для проверки функциональности класса JwtUserDetailsService.
+ * Этот класс загружает и возвращает объект типа UserDetails по имени пользователя, используя метод
+ * loadUserByUsername().
+ */
+public class JwtUserDetailsServiceTest {
+
+    @Mock
+    private UserServiceClient userServiceClient;
+
+    private JwtUserDetailsService userDetailsService;
+
+    @BeforeEach
+    public void setup() {
+        // Инициализация mock-объектов и тестируемого объекта перед каждым тестом.
+        MockitoAnnotations.openMocks(this);
+        userDetailsService = new JwtUserDetailsService(userServiceClient);
+    }
+
+    /**
+     * Проверяет, что метод loadUserByUsername() возвращает объект UserDetails по полученному имени
+     * пользователя username.
+     */
+    @Test
+    public void testLoadUserByUsername() {
+        // Создание тестовых данных.
+        String username = "Bob_Smith";
+
+        User user = new User(
+                1,
+                username,
+                "pass132456",
+                "Bob",
+                "Smith",
+                "bob@gmail.com",
+                "ACTIVE",
+                Set.of("ROLE_USER")
+        );
+
+        // Установка поведения mock-объекта userServiceClient.
+        Mockito.when(userServiceClient.getUserByUsername(username)).thenReturn(user);
+
+        // Ожидаемый результат.
+        JwtUser expectedJwtUser = JwtUserFactory.created(user);
+
+        // Вызов тестируемого метода.
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // Проверка результата.
+        Assertions.assertEquals(expectedJwtUser, userDetails);
+    }
+
+    /**
+     * Проверяет, что метод loadUserByUsername() выбрасывает исключение UsernameNotFoundException, если пользователь
+     * не был найден по имени username.
+     */
+    @Test
+    public void testLoadUserByUsername_UserNotFound() {
+        // Создание тестовых данных.
+        String username = "Bob_Smith";
+
+        // Установка поведения mock-объекта.
+        Mockito.when(userServiceClient.getUserByUsername(username)).thenReturn(null);
+
+        // Проверка, что исключение UserNotFoundException выбрасывается при вызове метода loadUserByUsername().
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            userDetailsService.loadUserByUsername(username);
+        });
+    }
+}
