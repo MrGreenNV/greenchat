a76a6bc9a8abd2f4ad4ca737ef1f373182b85f8e
MrGreenNV <averkievnv@gmail.com>
2023-07-15 12:54:57 +0300

    add JwtUserFactory.java

---
+++ src/main/java/ru/averkiev/greenchat_auth/security/JwtUserFactory.java
@@ -0,0 +1,53 @@
+package ru.averkiev.greenchat_auth.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import ru.averkiev.greenchat_auth.models.User;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Класс предоставляет статические сетоды для создания объекта JwtUser из объекта User и преобразования ролей
+ * в коллекцию GrantedAuthority. Этот класс упрощает процесс создания JwtUser для использования в генерации и проверке
+ * JWT токенов в greenchat.
+ * @author mrGreenNV
+ */
+public class JwtUserFactory {
+
+    private JwtUserFactory() {
+
+    }
+
+    /**
+     * Создаёт и возвращает щбъект JwtUser на основе переданного объекта User. Преобразует поля объекта User
+     * в соответствующие поля JwtUser и преобразует роли пользователя в коллекцию объектов GrantedAuthority.
+     * @param user - переданный пользователь, которого необходимо преобразовать в объект JwtUser
+     * @return - объект JwtUser, в которого преобразован User
+     */
+    public static JwtUser created(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getEmail(),
+                user.getStatus().equals("ACTIVE"),
+                mapToGrantedAuthorities(new HashSet<>(user.getRoles()))
+        );
+    }
+
+    /**
+     * Преобразует множество строковых ролей пользователя в множество объектов GrantedAuthority. Использует
+     * метод stream() для преобразования каждой строки в объект SimpleGrantedAuthority и собирает результаты в
+     * новое множество с помощью Collectors.toSet().
+     * @param userRoles - список String ролей пользователя в структуре Set.
+     * @return - список SimpleGrantedAuthority в структуре Set ролей пользователя.
+     */
+    private static Set<GrantedAuthority> mapToGrantedAuthorities(Set<String> userRoles) {
+        return userRoles.stream()
+                .map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+    }
+}
