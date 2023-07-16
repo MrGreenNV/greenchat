f9a3bbe20c261964974315319e53964bf1b71bef
MrGreenNV <averkievnv@gmail.com>
2023-07-17 00:31:01 +0300

    add AuthException.java

---
+++ src/main/java/ru/averkiev/greenchat_auth/exceptions/AuthException.java
@@ -0,0 +1,12 @@
+package ru.averkiev.greenchat_auth.exceptions;
+
+/**
+ * Класс представляет собой исключение, которое возникает в случае, когда пароль из JwtRequest не совпадает с
+ * паролем, сохраненным в базе данных.
+ * @author mrGreenNV
+ */
+public class AuthException extends RuntimeException {
+    public AuthException(String msg) {
+        super(msg);
+    }
+}
