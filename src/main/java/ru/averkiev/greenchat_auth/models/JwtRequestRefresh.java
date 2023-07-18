b1dda7efa7faa3fcf8d602e5555a38301c1988cf
MrGreenNV <averkievnv@gmail.com>
2023-07-18 10:29:58 +0300

    add JwtRequestRefresh.java

---
+++ src/main/java/ru/averkiev/greenchat_auth/models/JwtRequestRefresh.java
@@ -0,0 +1,15 @@
+package ru.averkiev.greenchat_auth.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Класс представляет собой модель для передачи в запросе JWT refresh токена.
+ * @author mrGreenNV
+ */
+@Getter
+@Setter
+public class JwtRequestRefresh {
+
+    private String refreshToken;
+}
